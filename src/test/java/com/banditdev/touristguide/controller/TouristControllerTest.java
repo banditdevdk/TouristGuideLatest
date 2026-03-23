7aae4ba58a82662edeefd9a67716b48935662a88
davidek-d <dajo1002@stud.ek.dk>
2026-03-23 13:22:00 +0100

    pom og test lavet og burde være klar til at blive lavet

--- src/test/java/com/banditdev/touristguide/controller/TouristControllerTest.java
+++ src/test/java/com/banditdev/touristguide/controller/TouristControllerTest.java
@@ -1,22 +1,13 @@
 package com.banditdev.touristguide.controller;
 
-import com.banditdev.touristguide.model.TouristAttraction;
 import com.banditdev.touristguide.service.TouristService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.List;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(TouristController.class)
 class TouristControllerTest {
@@ -27,4 +18,11 @@ class TouristControllerTest {
     @MockitoBean
     private TouristService touristService;
 
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 }
