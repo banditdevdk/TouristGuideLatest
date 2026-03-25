02edece62636dfe45c3ed5d0e296e67718b9ecfb
davidek-d <dajo1002@stud.ek.dk>
2026-03-25 11:14:02 +0100

    applications tests lavet og fixet

---
+++ src/test/java/com/banditdev/touristguide/TouristGuideApplicationTests.java
@@ -0,0 +1,16 @@
+package com.banditdev.touristguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TouristGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
