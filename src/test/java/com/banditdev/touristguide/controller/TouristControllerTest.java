9298cd8b231a905c79a0c694630e9c285627105a
davidek-d <dajo1002@stud.ek.dk>
2026-03-25 11:01:18 +0100

    lavet en test properties fil til at bruge h2 database, og brugt activeprofiles i test

--- src/test/java/com/banditdev/touristguide/controller/TouristControllerTest.java
+++ src/test/java/com/banditdev/touristguide/controller/TouristControllerTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(TouristController.class)
+@ActiveProfiles("test")
 class TouristControllerTest {
 
     @Autowired
