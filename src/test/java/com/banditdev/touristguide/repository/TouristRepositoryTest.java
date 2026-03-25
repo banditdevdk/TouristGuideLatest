032c0e8a9a3ebcbe3809caa5c1bb0a30a33323d3
davidek-d <dajo1002@stud.ek.dk>
2026-03-25 12:12:09 +0100

    repo test 1 test lavet

--- src/test/java/com/banditdev/touristguide/repository/TouristRepositoryTest.java
+++ src/test/java/com/banditdev/touristguide/repository/TouristRepositoryTest.java
@@ -1,10 +1,15 @@
 package com.banditdev.touristguide.repository;
 
+import com.banditdev.touristguide.model.TouristAttraction;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
 @SpringBootTest
@@ -12,7 +17,16 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TE
 @Sql(scripts = "classpath:sql/h2database.sql", executionPhase = BEFORE_TEST_METHOD)
 class TouristRepositoryTest {
 
+    @Autowired
+    private TouristRepository repository;
+
     @Test
     void findAll() {
+        List<TouristAttraction> test = repository.findAll();
+
+        // der burde være 6 attractions i H2 SQL scriptet - classpath:sql/h2database.sql
+        assertThat(test.size()).isEqualTo(6);
+        assertThat(test.getFirst().getName()).isEqualTo("Den Lille Havfrue");
+        assertThat(test.getFirst().getId()).isEqualTo(1);
     }
 }
